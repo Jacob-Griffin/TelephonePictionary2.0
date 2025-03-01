798a986b74d15076cb43274c85c61b3f034bd92a
jacob-griffin <jacob@griffinspride.com>
2025-03-01 10:56:37 -0800

    fix theme generator

--- packages/byfo-app-themes/generator/generate-theme.node.js
+++ packages/byfo-app-themes/generator/generate-theme.node.js
@@ -83,7 +83,7 @@ export type { ThemeId, Theme };
 export * from './bases/ThemeSpec';
 export * from './bases/ThemeId';
 export { themes };
-export { default as applicationRules } from './bases/applicationRules';
+export { default as applicationRules, installRootStyles } from './bases/applicationRules';
 export default themes;
 `;
 
