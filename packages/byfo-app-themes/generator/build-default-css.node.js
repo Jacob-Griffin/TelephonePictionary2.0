c97277901432f79be6782d3088915d07d9f48404
jacob-griffin <jacob@griffinspride.com>
2025-03-21 18:09:55 -0700

    Add asset folder safety

--- packages/byfo-app-themes/generator/build-default-css.node.js
+++ packages/byfo-app-themes/generator/build-default-css.node.js
@@ -1,7 +1,11 @@
-import { writeFileSync } from 'fs';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { themeToString } from '../dist/bases/themeToString.js';
 import light from '../dist/themes/builtin/light.js';
 import dark from '../dist/themes/builtin/dark.js';
 import applicationRuleText from '../dist/bases/applicationRuleText.js';
 const content = `:root{${themeToString(light)}} @media(prefers-color-scheme:dark){:root{${themeToString(dark)}}} ${applicationRuleText}`;
-writeFileSync(import.meta.dirname.replace('/generator', '/dist/assets/initial.css'), content);
+const assetsDir = import.meta.dirname.replace('/generator', '/dist/assets/');
+if (!existsSync(assetsDir)) {
+  mkdirSync(assetsDir);
+}
+writeFileSync(assetsDir + 'initial.css', content);
